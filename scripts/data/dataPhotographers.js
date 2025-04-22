39a2456411ca4e875299ed3dcfdac68c910afc0b
Mathieu FENOUIL <matfen2.0@outlook.fr>
2025-04-22 21:07:52 +0200

    Récupération des données JSON
    - Ajout de la fonction getPhotographers pour récupérer les données des photographes depuis le JSON
    - Intégration des données dans le template des photographes
    - Affichage des cartes des photographes sur la page d'accueil
    - Gestion des erreurs pour les requêtes fetch"

---
+++ scripts/data/dataPhotographers.js
@@ -0,0 +1,15 @@
+const getPhotographers = async (url)=> {
+        try {
+          const api = await fetch(url);
+          // console.log(api);
+          if (api.ok) {
+            const res = await api.json();
+            const photographers = res.photographers;
+            return photographers;
+          } else {
+            throw new Error("data photographers not found")
+          }
+        } catch (err) {
+          console.error(err.message);
+        }
+}
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,115 @@
-function photographerTemplate(data) {
-    const { name, portrait } = data;
-
+const photographerTemplate = (data) => {
+    const { city, country, id, name, portrait, price, tagline } = data;
+  
     const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+  
+    const getUserCardDOM = () => {
+      //créer le lien qui contient image et le nom du photographe
+      const link = document.createElement("a");
+      const article = document.createElement("article");
+      const img = document.createElement("img");
+  
+      link.setAttribute("href", `photographer.html?id=${id}`);
+  
+      img.setAttribute("src", picture);
+      img.setAttribute("id", id);
+      img.setAttribute("alt", `Portrait de ${name}`);
+      const h2 = document.createElement("h2");
+      h2.textContent = name;
+  
+      link.append(img, h2);
+  
+      //créer la description du photographe
+      const descriptionBox = document.createElement("div");
+      const h3 = document.createElement("h3");
+      const p1 = document.createElement("p");
+      const p2 = document.createElement("p");
+  
+      descriptionBox.classList.add("description");
+      descriptionBox.setAttribute("tabindex", "0")
+      h3.classList.add("country");
+      p1.classList.add("tagline");
+      p2.classList.add("price");
+  
+      h3.textContent = `${city}, ${country}`;
+      p1.textContent = tagline;
+      p2.textContent = `${price}€/jour`;
+  
+      descriptionBox.append(h3, p1, p2);
+      article.append(link, descriptionBox);
+  
+      return article;
     }
-    return { name, picture, getUserCardDOM }
-}
+  
+    const cardPhotographer = () => {
+      const btnContact = document.querySelector(".contact_button");
+  
+      const photographInfo = document.createElement("div");
+      photographInfo.classList.add("photograph-info");
+  
+      const h1 = document.createElement("h1");
+      h1.classList.add("photograph-name");
+      h1.textContent = name;
+  
+      const h2 = document.createElement("h2");
+      h2.classList.add("photograph-country");
+      h2.textContent = city + ", " + country;
+  
+      const p = document.createElement("p");
+      p.classList.add("photograph-tagline");
+      p.textContent = tagline;
+  
+      const imgContainer = document.createElement("div");
+      imgContainer.classList.add("img-container");
+  
+      const img = document.createElement("img");
+      img.classList.add("photograph-image");
+      img.setAttribute("src", picture);
+      img.setAttribute("alt", name);
+      imgContainer.append(img);
+  
+      photographInfo.append(h1, h2, p);
+      btnContact.before(photographInfo);
+      btnContact.after(imgContainer);
+    }
+  
+    const cardPrice = () => {
+      const main = document.querySelector("#main");
+  
+      const cardPrice = document.createElement("section");
+      cardPrice.classList.add("card-price");
+  
+      const sumPrice = document.createElement("h3");
+      sumPrice.classList.add("sum-likes");
+      const iconheart = document.createElement("i");
+      iconheart.classList.add("fa-solid", "fa-heart");
+  
+      const pricePerDay = document.createElement("h3");
+      pricePerDay.classList.add("price-per-day");
+      pricePerDay.textContent = price + "€/jour";
+  
+      cardPrice.append(sumPrice, iconheart, pricePerDay);
+      main.appendChild(cardPrice);
+    }
+  
+    const nameForForm = () => {
+      const btnClose = document.querySelector(".btn-close");
+      const h2 = document.createElement("h2");
+      h2.textContent = "Contactez-moi " + name;
+      btnClose.before(h2);
+    }
+  
+    return {
+      city,
+      country,
+      id,
+      name,
+      portrait,
+      price,
+      tagline,
+      getUserCardDOM,
+      cardPhotographer,
+      cardPrice,
+      nameForForm,
+    };
+  }
